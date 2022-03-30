754923ec7778add55ae161a9bc44853da0e2c1c0
Junwoo Kim <junwoo.kim@pmgrow.co.kr>
2022-03-30 20:33:20 +0900

    sentinel config

--- workers/config.js
+++ workers/config.js
@@ -1,7 +1,14 @@
 const Bull = require('bull')
 
 const connectQueue = (name) => new Bull(name, {
-    redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST }
+    redis: {  // port: process.env.REDIS_PORT, host: process.env.REDIS_HOST 
+      name: 'turple'
+      sentinels: [
+        {host: 'redis-1', port: 26379},
+        {host: 'redis-2', port: 26379},
+        {host: 'redis-3', port: 26379},
+      ]
+    }
 })
 
 module.exports = {connectQueue }
